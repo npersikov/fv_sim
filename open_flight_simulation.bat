a86a0c9bb3047d39854c6e8e5b0201d854bae6a8
Nikita <persikov@usc.edu>
2022-02-21 16:48:09 -0800

    Visualization fix, angular kinematics debugging
    The ground scenery now appears, and the plane behaves as expected for given inputs

--- open_flight_simulation.bat
+++ open_flight_simulation.bat
@@ -5,4 +5,4 @@ matlab -r "edit 'C:\Users\Nikita\Documents\USC\Homework\Spring 2022\Flight Vehic
 
 cd "C:\Program Files\FlightGear 2020.3"
 SET FG_ROOT=C:\Program Files\FlightGear 2020.3\data
-bin\fgfs.exe --aircraft=ExperimentalCarrier --fdm=network,localhost,5501,5502,5503 --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --enable-freeze --airport=KSFO --runway=10L --altitude=328 --heading=0 --offset-distance=0 --offset-azimuth=0
+bin\fgfs.exe --aircraft=ExperimentalCarrier --fdm=network,localhost,5501,5502,5503 --fog-fastest --disable-clouds --start-date-lat=2004:00:01:09:00:00 --disable-sound --in-air --enable-freeze --terrain-engine=pagedLOD --lod-levels="1 3 5 7 9" --enable-random-objects --enable-terrasync 
--- run_simulation.m
+++ run_simulation.m
@@ -5,13 +5,13 @@ close all
 
 % Add all custom libraries to the workspace
 addpath('C:\Users\Nikita\Documents\USC\Homework\Spring 2022\Flight Vehicle Stability and Control\fv_sim/fv_sim/user_defined_libraries');
-% system('run_flightgear.bat');
+plots_on = false;
 
 %% Simulation Setup
 
 % Constants. TODO: move these out into their own folder
 re_m            = 6378000; % Radius of Earth
-g_mps2          = 0;
+g_mps2          = 9.81;
 
 % Simulation Parameters
 flight_time_s   = 1000; % Time used to set up simulink simulation %TODO connect this to Simulink
@@ -26,19 +26,24 @@ J = [900, 0, 0;
 % ===== Initial Conditions =====
 
 % Vehicle State
-ExE_BfromE_0_m  = [re_m+6000; 0; 0]; % Starting vector of body in ECEF frame and CS
+lat_d           = 35.28; % N35.28
+lon_d           = -115;  % W115;
+ground_level_m  = 7968;  % Found through trial and error for N35.28 W115
+altitude_m      = 500; 
+
+ExE_BfromE_0_m  = lla2ecef([lat_d, lon_d, ground_level_m + altitude_m]); % SoCal
 EvB_BfromE_mps  = [0; 0; 0]; % Velocity of the body in ECEF frame in the body CS
-omega_BwrtN_dps = [0; 0; 0.01]; % roll pitch yaw rates, or phi theta psi rates. (IE, rotate about the down axis)
+omega_BwrtN_dps = [0; 0; 0]; % roll pitch yaw rates, or phi theta psi rates. (IE, rotate about the down axis)
 omega_BwrtN_rps = deg2rad(omega_BwrtN_dps);
 omega_pure_quat = [0; omega_BwrtN_rps]';
 
 % Frames and Coordinate Systems
-ll_NfromE_deg               = [0, 0]; % Lat lon and euler angles for DCMs. Note: N is North East Down; Need inv of this DCM
+ll_NfromE_deg               = [lat_d, lon_d]; % Lat lon and euler angles for DCMs. Note: N is North East Down; Need inv of this DCM
 ll_NfromE_rad               = deg2rad(ll_NfromE_deg); %TODO: figure out where to handle degree changes
 euler_angles_NfromB_0_deg   = [0; 0; 0]; % Body frame same as NED. Note: C_EfromB is found in simulink. NOTE: must be ZYX rotation!
 euler_angles_NfromB_0_rad   = deg2rad(euler_angles_NfromB_0_deg);
 omega_EwrtI_rps_0           = [0; 0; 0]; % rotation of Earth
-omega_BwrtI_rps_0           = omega_BwrtN_dps + omega_EwrtI_rps_0; % since initial N to E velocity is 0;0;0
+omega_BwrtI_rps_0           = omega_BwrtN_rps + omega_EwrtI_rps_0; % since initial N to E velocity is 0;0;0
 
 % ===== Simulation =====
 
@@ -50,29 +55,32 @@ tout                    = simout.tout;
 ExE_BfromE_m            = simout.ExE_BfromE_m;
 euler_angles_NfromB_deg = rad2deg(simout.euler_angles_NfromB_rad);
 
+if plots_on
+    
+    % ===== Post Processing =====
+    problem = "Problem 2b: ";
+    figure(1)
+    title("Unique initial conditions");
+    subplot(1,3,1);
+    plot(tout, ExE_BfromE_m(:,1));
+    title("X position vs time");
+    subplot(1,3,2);
+    plot(tout, ExE_BfromE_m(:,2));
+    title("Y position vs time");
+    subplot(1,3,3);
+    plot(tout, ExE_BfromE_m(:,3));
+    title("Z position vs time");
+    
+    figure(2)
+    title("Angular position");
+    subplot(1,3,1);
+    plot(tout, euler_angles_NfromB_deg(:,1));
+    title("Rotation about Z (degrees) vs time (s)");
+    subplot(1,3,2);
+    plot(tout, euler_angles_NfromB_deg(:,2));
+    title("Rotation about Y (degrees) vs time (s)");
+    subplot(1,3,3);
+    plot(tout, euler_angles_NfromB_deg(:,3));
+    title("Rotation about X (degrees) vs time (s)");
 
-% ===== Post Processing =====
-problem = "Problem 2b: ";
-figure(1)
-title("Unique initial conditions");
-subplot(1,3,1);
-plot(tout, ExE_BfromE_m(:,1));
-title("X position vs time");
-subplot(1,3,2);
-plot(tout, ExE_BfromE_m(:,2));
-title("Y position vs time");
-subplot(1,3,3);
-plot(tout, ExE_BfromE_m(:,3));
-title("Z position vs time");
-
-figure(2)
-title("Angular position");
-subplot(1,3,1);
-plot(tout, euler_angles_NfromB_deg(:,1));
-title("Rotation about Z (degrees) vs time (s)");
-subplot(1,3,2);
-plot(tout, euler_angles_NfromB_deg(:,2));
-title("Rotation about Y (degrees) vs time (s)");
-subplot(1,3,3);
-plot(tout, euler_angles_NfromB_deg(:,3));
-title("Rotation about X (degrees) vs time (s)");
+end
