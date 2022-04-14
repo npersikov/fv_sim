f32efcfbcaefd854d604a408ad46bde7a1312ee2
Nikita <persikov@usc.edu>
2022-04-13 20:04:09 -0700

    Error fixing in progress

--- get_lqr_k.m
+++ get_lqr_k.m
@@ -10,14 +10,19 @@
 %
 % Finally, you need x, which is the deviation from the trim state.
 function K = get_lqr_k(A, B, C, Q, R, x, P_0, S_0)
+    ps_0 = set_ps(P_0, S_0);
+    n = length(P_0);
+    m = length(S_0);
     % Find P and S matrices using glorified trial and error
-    [P, S] = fminsearch(@(P, S)lyap_sys(A, B, C, P, Q, R, S, x), [P_0, S_0]);
+    ps = fminsearch(@(ps)lyap_sys(A, B, C, Q, R, x, n, m, ps), ps_0);
+    [P, S] = get_ps(ps, n, m);
     % Calculate K now that all matrices are known
     K = calc_K(B, C, P, R, S);
 end
 
 % The functions that must be solved for P and S
-function [f1, f2] = lyap_sys(A, B, C, P, Q, R, S, x)
+function [f1, f2] = lyap_sys(A, B, C, Q, R, x, n, m, ps)
+    [P, S] = get_ps(ps, n, m);
     K = calc_K(B, C, P, R, S);
     f1 = A'*P + P*A + Q + C'*K'*R*K*C;
     f2 = A*S + S*A' + x;
@@ -26,4 +31,43 @@ end
 % Utility function to calculate K from other matrices
 function K = calc_K(B, C, P, R, S)
     K = R\B'*P*S*C'/(C*S*C'); % Definition of the LQR gain matrix
+end
+
+function [P, S] = get_ps(ps, n, m)
+    % Throw error if ps has the wrong number of elements based on the
+    % dimensions of P and S, namely nxn and mxm
+    if length(ps) ~= n^2 + m^2
+        error("ps has an improper amount of elements");
+    end
+    
+    % Allocate P and S and set them to their proper dimensions
+    P = zeros(n);
+    S = zeros(m);
+    for i = 1:1:length(ps)
+        if i <= n^2
+            % Get the row of P we are on for current i
+            row = floor((i-1)/n)+1; 
+            % i minus number of full rows passed should give the column
+            col = i - floor((i-1)/n)*n ;
+            P(row, col) = ps(i); % Set the value in the right place in P
+        else
+            % Get the row of S we are on for current i
+            row = floor((i-n^2-1)/m)+1; 
+            % i minus number of full rows passed should give the column
+            col = i-n^2 - floor((i-n^2-1)/m)*m ;
+            S(row, col) = ps(i); % Set the value in the right place in S
+        end
+    end
+end
+
+function ps = set_ps(P, S)
+    ps = [];
+    n = length(P);
+    m = length(S);
+    for i = 1:1:n
+        ps = [ps, P(i, 1:n)];
+    end
+    for i = 1:1:m
+        ps = [ps, S(i, 1:m)];
+    end
 end
