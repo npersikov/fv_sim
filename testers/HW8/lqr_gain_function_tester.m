bb2bb3c0f3873457f3010d7cc9106c054dff9487
Nikita <persikov@usc.edu>
2022-04-13 20:04:09 -0700

    Error fixing in progress

--- testers/HW8/lqr_gain_function_tester.m
+++ testers/HW8/lqr_gain_function_tester.m
@@ -14,14 +14,55 @@ dx = x_0 - x; % offset from trim point
 
 % Classical controls outputs
 [num, den] = ss2tf(A, B, C, B*0);
-rlocus(num(1, :), den);
+% rlocus(num(1, :), den);
 
 % Weight Matrices for LQR controller
 Q = 3*eye(2); % Make this larger for aggressive correction
 R = eye(2);
 
 % Initial Guesses for P and S
-P_0 = ones(length(x));
-S_0 = [1];
+% P_0 = ones(length(x));
+% S_0 = [1];
+P_0 = [2, 3; 4, 5];
+S_0 = [1, 5.5;3, 5.6];
 
 K = get_lqr_k(A, B, C, Q, R, x, P_0, S_0);
+
+function ps = set_ps(P, S)
+    ps = [];
+    n = length(P);
+    m = length(S);
+    for i = 1:1:n
+        ps = [ps, P(i, 1:n)];
+    end
+    for i = 1:1:m
+        ps = [ps, S(i, 1:m)];
+    end
+end
+
+function [P, S] = get_ps(ps, n, m)
+    % Throw error if ps has the wrong number of elements based on the
+    % dimensions of P and S, namely nxn and mxm
+    if length(ps) ~= n^2 + m^2
+        error("ps has an improper amount of elements");
+    end
+    
+    % Allocate P and S and set them to their proper dimensions
+    P = zeros(n);
+    S = zeros(m);
+    for i = 1:1:length(ps)
+        if i <= n^2
+            % Get the row of P we are on for current i
+            row = floor((i-1)/n)+1; 
+            % i minus number of full rows passed should give the column
+            col = i - floor((i-1)/n)*n ;
+            P(row, col) = ps(i); % Set the value in the right place in P
+        else
+            % Get the row of S we are on for current i
+            row = floor((i-n^2-1)/m)+1; 
+            % i minus number of full rows passed should give the column
+            col = i-n^2 - floor((i-n^2-1)/m)*m ;
+            S(row, col) = ps(i); % Set the value in the right place in S
+        end
+    end
+end
